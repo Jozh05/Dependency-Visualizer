64d75284fe4fd5f050e9bee55ffb40210e4dd374
Mikhail Krysin <misha.krysin@inbox.ru>
2024-11-14 18:17:43 +0300

    Удален мусор

--- libarchive_build/cat/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Mikhail Krysin/source/repos/libarchive-3.7.7/cat
-# Build directory: C:/Users/Mikhail Krysin/source/repos/HW_2_config/libarchive_build/cat
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
--- libarchive_build/libarchive/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Mikhail Krysin/source/repos/libarchive-3.7.7/libarchive
-# Build directory: C:/Users/Mikhail Krysin/source/repos/HW_2_config/libarchive_build/libarchive
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
